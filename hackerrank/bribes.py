e2d5c9d0fc5d972df8e8ba4f3c35933d78c01356
Mariusz Jakubowski <mariusz.jakubowski.79@gmail.com>
2020-02-02 23:50:02 +0100

    bribes

--- hackerrank/bribes.py
+++ hackerrank/bribes.py
@@ -9,35 +9,49 @@ import sys
 # Complete the minimumBribes function below.
 def minimumBribes(q):
   count = 0
+  qq = [ x for x in range(1, len(q) + 1)]
   for i, v in enumerate(q):
-    if v == i + 2:
+    if (q[i] == qq[i]):
+      # print("%d no change" % i)
+      continue
+    elif (i < len(q) - 1 and q[i] == qq[i + 1]):
       count += 1
-    if v == i + 3:
+      qq[i + 1] = qq[i]
+      qq[i] = q[i]
+      # print("%d ch 1 " % i)
+      # print(qq)
+    elif (i < len(q) - 2 and q[i] == qq[i + 2]):
       count += 2
-    if v > i + 3:
-      count = -1
-      break
-  if count == -1:
-    print("Too chaotic")
-  else:
-    print(count)
+      qq[i + 2] = qq[i + 1]
+      qq[i + 1] = qq[i]
+      qq[i] = q[i]
+      # print("%d ch 2 " % i)
+      # print(qq)
+    else:
+      print("Too chaotic")
+      return
+
+  print(count)
   
 
 if __name__ == '__main__':
-    minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])
-    1 2 3 4 5 6 7 8
-    1 2 3 5 4 6 7 8
-    1 2 5 3 4 6 7 8
-    1 2 5 3 4 7 6 8
-    1 2 5 3 7 4 6 8
-    1 2 5 3 7 4 8 6
-    1 2 5 3 7 8 4 6
-    1 2 5 3 7 8 6 4
-    # t = int(input())
+    # minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])
+    # minimumBribes([2, 1, 5, 3, 4])
+    # minimumBribes([2, 5, 1, 3, 4])
+    # 1 2 3 4 5 6 7 8
+    # 1 2 3 5 4 6 7 8
+    # 1 2 5 3 4 6 7 8
+    # 1 2 5 3 4 7 6 8
+    # 1 2 5 3 7 4 6 8
+    # 1 2 5 3 7 4 8 6
+    # 1 2 5 3 7 8 4 6
+    # 1 2 5 3 7 8 6 4
 
-    # for t_itr in range(t):
-    #     n = int(input())
+    t = int(input())
 
-    #     q = list(map(int, input().rstrip().split()))
+    for t_itr in range(t):
+        n = int(input())
 
-    #     minimumBribes(q)
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
