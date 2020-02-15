b44c4fdd7615347bf12c402f05585a2c747306e0
Mariusz Jakubowski <mariusz.jakubowski.79@gmail.com>
2020-02-15 23:48:53 +0100

    alternate characters

---
+++ hackerrank/alt_chars.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    prev = ''
+    deleted = 0
+    for c in s:
+        if c == prev:
+            deleted += 1
+        else:
+            prev = c
+    #print(deleted)
+    return deleted
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
